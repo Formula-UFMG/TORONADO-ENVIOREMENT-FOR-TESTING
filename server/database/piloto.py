2faf70c8c4134cf80e58d41e6daaac49a4e1003a
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-20 18:16:25 -0300

    0.54.0
    função para coletar as informações dos pilotos

--- server/database/piloto.py
+++ server/database/piloto.py
@@ -1,2 +1,34 @@
-def creat_piloto(piloto):
-    pass
+import sys
+import os
+
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(root_path)
+
+from mysql.connector import Error
+
+from database import connection
+from error_reporter import send_email
+from server.classes import piloto
+TABLE = "TEFT.membro"
+
+def get_pilotos():
+    comando = ("SELECT * FROM {} ".format(TABLE)) # comando sql 
+    verificador, cursor, con = connection.connect_to_db() # coleta as informações para a conexão com o banco 
+    if verificador == True:
+        try:
+            # tenta executar o comando 
+            cursor.execute(comando) 
+            linhas = cursor.fetchall()
+            # verifica a informação
+            saida = [] 
+            for linha in linhas:
+                saida.append(piloto.Pilotos(linha[0],linha[1],linha[2],linha[3],linha[4]))
+            var_login = saida
+        except Error as e: # 
+            verificador = False
+            send_email(e)
+        # finaliza a conexão com o banco 
+        connection.close_connect_to_bd(cursor,con)
+        return verificador, var_login
+    else:
+        return verificador, None
