de3433ef61045895646240114e7af2e4359f45bd
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2025-01-03 04:13:30 -0300

    0.68.1
    Modificações na classe circuito

--- server/classes/circuito.py
+++ server/classes/circuito.py
@@ -1,5 +1,13 @@
+import os
+import sys
+
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(root_path)
+
+import path_manager 
+
 class Circuito:
-    def __init__(self, id_circuito, nome, tempo_descolcamento, KM, curvas, cones, local):
+    def __init__(self, id_circuito, nome, tempo_descolcamento, KM, curvas, cones, local,data_criacao):
         self.id_circuito = id_circuito
         self.nome = nome
         self.tempo_descolcamento = tempo_descolcamento
@@ -7,12 +15,14 @@ class Circuito:
         self.curvas = curvas
         self.cones = cones
         self.local = local
+        self.data_criacao = data_criacao
         if id_circuito != None:
-            self.caminho = "circuito" + str(id_circuito) + ".PNG"
+            caminho = "circuito" + str(id_circuito) + ".png"
+            self.caminho = path_manager.join_path(path_manager.get_upload_path(),caminho)
     def modificar(self, nome, tempo_descolcamento, KM, curvas, cones, local):
         self.nome = nome
-        self.tempo_descolcamento = tempo_descolcamento
-        self.KM = KM
-        self.curvas = curvas
-        self.cones = cones
+        self.tempo_descolcamento = float(tempo_descolcamento)
+        self.KM = float(KM)
+        self.curvas = int(curvas)
+        self.cones = int(cones)
         self.local = local
