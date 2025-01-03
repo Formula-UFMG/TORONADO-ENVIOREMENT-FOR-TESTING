73a21ec82de89676f7e9f150dc6969232b7e4401
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2025-01-03 04:15:16 -0300

    0.68.4
    modificação da classe teste

--- server/classes/teste.py
+++ server/classes/teste.py
@@ -1,5 +1,5 @@
 class Teste:
-    def __init__(self, N_teste, pilotos, id_objetivos, N_voltas, inicio, fim, almoco, data, id_prototipo, id_circuito):
+    def __init__(self, N_teste, pilotos, id_objetivos, N_voltas, inicio, fim, almoco, data, id_prototipo, id_circuito, n_testes):
         self.N_teste = N_teste
         self.pilotos = pilotos
         self.id_objetivos = id_objetivos
@@ -10,6 +10,7 @@ class Teste:
         self.data = data
         self.id_prototipo = id_prototipo
         self.id_circuito = id_circuito
+        self.n_testes = n_testes
         if self.N_teste != None:
             self.documento = "pre_teste" + str(self.N_teste) + ".pdf"
         else:
