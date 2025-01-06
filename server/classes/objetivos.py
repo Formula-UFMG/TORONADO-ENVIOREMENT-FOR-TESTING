0968bcf91f04f5a6068496a4391e25c61970e571
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2025-01-06 02:43:40 -0300

    0.80.0
    classe para facilitar a manipulação dos objetivos

---
+++ server/classes/objetivos.py
@@ -0,0 +1,5 @@
+class Objetivos:
+    def __init__(self, id_metodologia, proposito, status):
+        self.id_metodologia = id_metodologia
+        self.proposito = proposito
+        self.status = status
