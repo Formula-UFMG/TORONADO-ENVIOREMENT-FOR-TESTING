20a89e65d81c9e6b5dc379aa2c1b88c8a42a3eec
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2025-02-11 14:01:23 -0300

    0.87.0
    adição da função de adicionar nome, de formatação para a saida e transforma a classe em um dicionário

--- server/classes/piloto.py
+++ server/classes/piloto.py
@@ -5,3 +5,20 @@ class Pilotos:
         self.n_testes = n_testes
         self.email = email
         self.kms = kms
+    
+    def add_nome(self, nome):
+        self.nome = nome
+
+    def formato_saida(self, nome):
+        self.saida = str(self.id_piloto) + "|" + str(self.temporada)
+        self.nome = nome
+
+    def to_dict(self):
+        return {
+            "id_piloto": self.id_piloto,
+            "temporada": self.temporada,
+            "n_testes": self.n_testes,
+            "email": self.email,
+            "kms": self.kms,
+            "nome": self.nome,
+        }
