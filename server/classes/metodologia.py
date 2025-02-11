0a95ff06b28ebf3ff7facc9fafbb438407248cf1
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2025-02-11 13:59:23 -0300

    0.83.0
    modificação da classe metodologia

--- server/classes/metodologia.py
+++ server/classes/metodologia.py
@@ -1,5 +1,5 @@
 class Metodologia:
-    def __init__(self, id_metodologia, objetivo, N_pessoas, subgrupo, procedimento, N_voltas, temporada):
+    def __init__(self, id_metodologia, objetivo, N_pessoas, subgrupo, procedimento, N_voltas, temporada, status):
         self.id_metodologia = id_metodologia
         self.objetivo = objetivo
         self.N_pessoas = N_pessoas
@@ -7,10 +7,25 @@ class Metodologia:
         self.procedimento = procedimento
         self.N_voltas = N_voltas
         self.temporada = temporada
+        self.status = status  
     
-    def modificar(self, objetivo, N_pessoas, subgrupo, procedimento, N_voltas):
+    def modificar(self, objetivo, N_pessoas, subgrupo, procedimento, N_voltas, status):
         self.objetivo = objetivo
         self.N_pessoas = N_pessoas
         self.subgrupo = subgrupo
         self.procedimento = procedimento
         self.N_voltas = N_voltas
+        if status != None:
+            self.status = status
+    
+    def to_dict(self):
+        return {
+            "id_metodologia": self.id_metodologia,
+            "objetivo": self.objetivo,
+            "N_pessoas": self.N_pessoas,
+            "subgrupo": self.subgrupo,
+            "procedimento": self.procedimento,
+            "N_voltas": self.N_voltas,
+            "temporada": self.temporada,
+            "status": self.status
+        }
